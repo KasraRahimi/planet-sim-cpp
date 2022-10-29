f1ba590eede8be2c2505a2b357992231840a223d
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-10-28 23:39:14 -0400

    added a distance calculating method

--- lib_dir/include/Planet.h
+++ lib_dir/include/Planet.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <cmath>
+#include <vector>
 
 class Planet {
 private:
     double x_, y_, dx_, dy_;
+
+    double distanceWith(Planet other);
+
+    std::vector<double> distanceWithInVector(Planet other);
 
 public:
     Planet(int x, int y);
--- lib_dir/source/Planet.cpp
+++ lib_dir/source/Planet.cpp
@@ -1,5 +1,19 @@
 #include "Planet.h"
 
+double Planet::distanceWith(Planet other) {
+    double deltax = other.x_ - x_;
+    double deltay = other.y_ - y_;
+
+    return sqrt(pow(deltax, 2) + pow(deltay, 2));
+}
+
+std::vector<double> Planet::distanceWithInVector(Planet other) {
+    double deltax = other.x_ - x_;
+    double deltay = other.y_ - y_;
+    std::vector<double> answer = { deltax, deltay };
+    return answer;
+}
+
 Planet::Planet(int x, int y) {
     x_ = x;
     y_ = y;
