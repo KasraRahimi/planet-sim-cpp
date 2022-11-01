6762b4d33b3dab7367a708572b36ff4df19b501e
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-11-01 16:37:32 -0400

    The main file now makes use of the game class to open a window

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <Game.h>
 #include <Planet.h>
 
 #define WIDTH 720
 #define HEIGHT 480
 
 int main() {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    bool isRunning = true;
-    while (isRunning) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderClear(renderer);
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    isRunning = false;
-                    break;
-                default:
-                    break;
-            }
-        }
-        SDL_RenderPresent(renderer);
+    Game game(SDL_INIT_EVERYTHING, "My Game", WIDTH, HEIGHT, SDL_RENDERER_ACCELERATED);
+    while (game.getIsRunning()) {
+        game.handleEvents();
     }
     return 0;
 }
