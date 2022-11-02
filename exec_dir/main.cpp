ee257322fdb7bcf71bd72e127af026b8dc2a8ad9
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-11-02 02:24:20 -0400

    Simple simulation of Earth and Moon system

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -7,19 +7,30 @@
 #define HEIGHT 480
 #define FPS 60
 #define TIME_PER_FRAME 1000/FPS
+#define SCALE 5/10e6
+#define TIMESTEP 3600
+#define EARTH_MASS 5.972e24
 
 int main() {
     Color red = { 255, 0, 0, 255 };
+    Color blue = { 0, 0, 255, 255 };
     Game game(SDL_INIT_EVERYTHING, "My Game", WIDTH, HEIGHT, SDL_RENDERER_ACCELERATED);
     Timer timer;
-    int x = 0, y = 0;
+
+    std::vector<Planet*> solarSystem;
+    Planet earth(EARTH_MASS, 20, 0, 0, 0, 0, &solarSystem);
+    Planet moon(0.012 * EARTH_MASS, 10, 384400000, 0, 0, -1022, &solarSystem);
+
     while (game.getIsRunning()) {
         timer.reset();
         game.clearRender();
         game.handleEvents();
-        game.drawRect(red, x, y, 100, 200);
-        x++;
-        y++;
+        for (std::vector<Planet*>::iterator planet = solarSystem.begin(); planet < solarSystem.end(); planet++)
+        {
+            Vec position = (*planet)->positionOnScreen(WIDTH, HEIGHT, SCALE);
+            game.drawCircle(blue, (*planet)->getRadius(), position.x, position.y);
+            (*planet)->move(TIMESTEP);
+        }
         if (timer.elapsedTime() < TIME_PER_FRAME)
             SDL_Delay(TIME_PER_FRAME - timer.elapsedTime());
     }
