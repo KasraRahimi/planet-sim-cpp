19e42dd13d0dd2e6467daeab6ba20f9b5671e095
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-11-01 20:33:01 -0400

    testing out FPS handling

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <Game.h>
 #include <Planet.h>
+#include <Timer.h>
 
 #define WIDTH 720
 #define HEIGHT 480
+#define FPS 60
+#define TIME_PER_FRAME 1000/FPS
 
 int main() {
+    Color red = { 255, 0, 0, 255 };
     Game game(SDL_INIT_EVERYTHING, "My Game", WIDTH, HEIGHT, SDL_RENDERER_ACCELERATED);
+    Timer timer;
+    int x = 0, y = 0;
     while (game.getIsRunning()) {
+        timer.reset();
         game.handleEvents();
+        game.drawRect(red, x, y, 100, 200);
+        x++;
+        y++;
+        if (timer.elapsedTime() < TIME_PER_FRAME)
+            SDL_Delay(TIME_PER_FRAME - timer.elapsedTime());
     }
     return 0;
 }
