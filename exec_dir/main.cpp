418bf1cee52f37508891817fea281bd69d4bd626
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-11-02 13:01:45 -0400

    Replaced Earth Moon system for planets from mercury to mars. Visual bug to be fixed

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -5,24 +5,29 @@
 
 #define WIDTH 720
 #define HEIGHT 480
-#define FPS 60
+#define FPS 1
 #define TIME_PER_FRAME 1000/FPS
-#define SCALE 5/10e6
-#define TIMESTEP 3600
+#define SCALE 1/10e8
+#define TIMESTEP 3600 * 12
 #define EARTH_MASS 5.972e24
 
 const Color RED = { 255, 0, 0, 255 };
 const Color GREEN = { 0, 255, 0, 255 };
 const Color BLUE = { 0, 0, 255, 255 };
 const Color GREY = { 128, 128, 128, 255 };
+const Color YELLOW = { 255, 255, 0, 255 };
+const Color ORANGE = { 255, 128, 0, 255 };
 
 int main() {
     Game game(SDL_INIT_EVERYTHING, "My Game", WIDTH, HEIGHT, SDL_RENDERER_ACCELERATED);
     Timer timer;
 
     std::vector<Planet*> solarSystem;
-    Planet earth(EARTH_MASS, 10, BLUE, 0, 0, 0, 0, &solarSystem);
-    Planet moon(0.012 * EARTH_MASS, 5, GREY, 384400000, 0, 0, -1022, &solarSystem);
+    Planet sun(1.989e30, 20, YELLOW, 0, 0, 0, 0, &solarSystem);
+    Planet mercury(0.055 * EARTH_MASS, 5, GREY, 60e9, 0, 0, -47e3, &solarSystem);
+    Planet venus(0.815 * EARTH_MASS, 12, ORANGE, 109e9, 0, 0, -35e3, &solarSystem);
+    Planet earth(EARTH_MASS, 10, BLUE, 150e9, 0, 0, -30e3, &solarSystem);
+    Planet mars(0.107 * EARTH_MASS, 8, RED, 225e9, 0, 0, -24e3, &solarSystem);
 
     while (game.getIsRunning()) {
         timer.reset();
@@ -31,7 +36,8 @@ int main() {
         for (std::vector<Planet*>::iterator planet = solarSystem.begin(); planet < solarSystem.end(); planet++)
         {
             Vec position = (*planet)->positionOnScreen(WIDTH, HEIGHT, SCALE);
-            game.drawCircle((*planet)->getColor(), (*planet)->getRadius(), position.x, position.y);
+            int radius = (*planet)->getRadius();
+            game.drawCircle((*planet)->getColor(), radius, position.x, position.y);
             (*planet)->move(TIMESTEP);
         }
         if (timer.elapsedTime() < TIME_PER_FRAME)
