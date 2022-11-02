355ff63f4880941e3b74e343287647f5b19040ab
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-11-02 10:55:36 -0400

    Changed color of Earth and Moon system

--- exec_dir/main.cpp
+++ exec_dir/main.cpp
@@ -11,15 +11,18 @@
 #define TIMESTEP 3600
 #define EARTH_MASS 5.972e24
 
+const Color RED = { 255, 0, 0, 255 };
+const Color GREEN = { 0, 255, 0, 255 };
+const Color BLUE = { 0, 0, 255, 255 };
+const Color GREY = { 128, 128, 128, 255 };
+
 int main() {
-    Color red = { 255, 0, 0, 255 };
-    Color blue = { 0, 0, 255, 255 };
     Game game(SDL_INIT_EVERYTHING, "My Game", WIDTH, HEIGHT, SDL_RENDERER_ACCELERATED);
     Timer timer;
 
     std::vector<Planet*> solarSystem;
-    Planet earth(EARTH_MASS, 10, 0, 0, 0, 0, &solarSystem);
-    Planet moon(0.012 * EARTH_MASS, 5, 384400000, 0, 0, -1022, &solarSystem);
+    Planet earth(EARTH_MASS, 10, BLUE, 0, 0, 0, 0, &solarSystem);
+    Planet moon(0.012 * EARTH_MASS, 5, GREY, 384400000, 0, 0, -1022, &solarSystem);
 
     while (game.getIsRunning()) {
         timer.reset();
@@ -28,7 +31,7 @@ int main() {
         for (std::vector<Planet*>::iterator planet = solarSystem.begin(); planet < solarSystem.end(); planet++)
         {
             Vec position = (*planet)->positionOnScreen(WIDTH, HEIGHT, SCALE);
-            game.drawCircle(blue, (*planet)->getRadius(), position.x, position.y);
+            game.drawCircle((*planet)->getColor(), (*planet)->getRadius(), position.x, position.y);
             (*planet)->move(TIMESTEP);
         }
         if (timer.elapsedTime() < TIME_PER_FRAME)
