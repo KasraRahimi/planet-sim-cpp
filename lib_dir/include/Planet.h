d5764d1ef68aae54d6b2fbef908db0fc829d2f39
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-11-02 02:23:09 -0400

    Added several methods for Planet class

--- lib_dir/include/Planet.h
+++ lib_dir/include/Planet.h
@@ -2,18 +2,28 @@
 #include <cmath>
 #include <vector>
 
+struct Vec { 
+    double x, y;
+    double getMagnitude();
+};
+
 class Planet {
 private:
-    double x_, y_, dx_, dy_;
+    double mass_, x_, y_, dx_, dy_;
+    int radius_;
+    const double G = 6.674e-11;
+    std::vector<Planet*>* planetSystem_;
 
-    double distanceWith(Planet other);
-
-    std::vector<double> distanceWithInVector(Planet other);
+    Vec distanceWith(Planet other);
+    Vec forceWith(Planet other);
+    Vec sumOfForces();
 
 public:
-    Planet(int x, int y);
-
+    Planet(double mass, int radius_, double x, double y, double dx, double dy, std::vector<Planet*>* system);
     double getX();
-
     double getY();
+    double getMass();
+    int getRadius();
+    void move(const double timestep);
+    Vec positionOnScreen(double width, double height, double scale);
 };
--- lib_dir/source/Planet.cpp
+++ lib_dir/source/Planet.cpp
@@ -1,24 +1,66 @@
 #include "Planet.h"
 
-double Planet::distanceWith(Planet other) {
-    double deltax = other.x_ - x_;
-    double deltay = other.y_ - y_;
-
-    return sqrt(pow(deltax, 2) + pow(deltay, 2));
+double Vec::getMagnitude() {
+    return sqrt((x * x) + (y * y));
 }
 
-std::vector<double> Planet::distanceWithInVector(Planet other) {
+Vec Planet::distanceWith(Planet other) {
     double deltax = other.x_ - x_;
     double deltay = other.y_ - y_;
-    std::vector<double> answer = { deltax, deltay };
-    return answer;
+    return { deltax, deltay };
 }
 
-Planet::Planet(int x, int y) {
+Vec Planet::forceWith(Planet other) {
+    Vec distance = distanceWith(other);
+    double forceMagnitude = G * mass_ * other.getMass() / pow(distance.getMagnitude(), 2);
+    Vec force;
+    force.x = forceMagnitude * distance.x / distance.getMagnitude();
+    force.y = forceMagnitude * distance.y / distance.getMagnitude();
+    return force;
+}
+
+Vec Planet::sumOfForces() {
+    double forceInX = 0, forceInY = 0;
+    for (std::vector<Planet*>::iterator planet = planetSystem_->begin(); planet < planetSystem_->end(); planet++) {
+        if ((*planet)->getX() == x_ || (*planet)->getY() == y_)
+            continue;
+        Vec force = forceWith(**planet);
+        forceInX += force.x;
+        forceInY += force.y;
+    }
+    return { forceInX, forceInY };
+}
+
+Planet::Planet(double mass, int radius, double x, double y, double dx, double dy, std::vector<Planet*>* system) {
     x_ = x;
     y_ = y;
+    dx_ = dx;
+    dy_ = dy;
+    mass_ = mass;
+    radius_ = radius;
+    system->push_back(this);
+    planetSystem_ = system;
 }
 
 double Planet::getX() { return x_; }
 
 double Planet::getY() { return y_; }
+
+double Planet::getMass() { return mass_; }
+
+int Planet::getRadius() { return radius_; }
+
+void Planet::move(const double timestep) {
+    x_ += dx_ * timestep;
+    y_ += dy_ * timestep;
+
+    Vec force = sumOfForces();
+    dx_ += force.x * timestep / mass_;
+    dy_ += force.y * timestep / mass_;
+}
+
+Vec Planet::positionOnScreen(double width, double height, double scale) {
+    double relativeX = x_ * scale + (width / 2);
+    double relativeY = y_ * scale + (height / 2);
+    return { relativeX, relativeY };
+}
